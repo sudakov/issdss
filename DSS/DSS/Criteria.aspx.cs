8c2631e03b3acc0c4fec3d54c8c1adda09efcb77
Psilon <pzixel@gmail.com>
2015-11-21 12:01:26 +0300

    Добавил шкалу для других

--- DSS/DSS/Criteria.aspx.cs
+++ DSS/DSS/Criteria.aspx.cs
@@ -37,6 +37,7 @@ namespace DSS.DSS
             _BTN_Add.Enabled = false;
             _BTN_Update.Enabled = false;
             _PNL_CriteriaDelete.Visible = false;
+            ScaleEditTd.Visible = false;
             using (SqlConnection Connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DSSConnectionString"].ConnectionString))
             {
                 SqlCommand Command = new SqlCommand("dbo.issdss_permission_Read", Connection);
@@ -71,6 +72,7 @@ namespace DSS.DSS
                             _TB_Ord.Enabled = true;
                             _BTN_Add.Enabled = true;
                             _BTN_Update.Enabled = true;
+                            ScaleEditTd.Visible = IsScaleVisible();
                             break;
                         case 24: _PNL_CriteriaDelete.Visible = true; break;
                     }
@@ -84,7 +86,7 @@ namespace DSS.DSS
                 string ID = Convert.ToString(Request.QueryString["id"]);
                 DrawTree(ID);
 
-                if (ID != null && ID != "")
+                if (!string.IsNullOrEmpty(ID))
                 {
                     TreeView1.SelectedNode.Value = ID;
                     TreeView1_SelectedNodeChanged(sender, e);
@@ -520,6 +522,25 @@ namespace DSS.DSS
         {
             _HL_EditMethod.NavigateUrl = "~/DSS/" + _DDL_AggregationMethodUrl.Items.FindByValue(_DDL_AggregationMethod.SelectedValue).Text + ".aspx?id=" + TreeView1.SelectedNode.Value;
             _PNL_Editor.Visible = true;
+
+            ScaleEditTd.Visible = IsScaleVisible();
+        }
+
+        private bool IsScaleVisible()
+        {
+            if (TreeView1.SelectedNode == null)
+                return false;
+            var critId = int.Parse(TreeView1.SelectedNode.Value);
+            using (var context = new DssDataContext())
+            {
+                bool hasChild = context.criterias.Any(x => x.parent_crit_id == critId);
+                var selectedItem = context.criterias.Single(x => x.id == critId);
+                bool isFuzzy = selectedItem.method_id == 7;
+                bool isScaleVisible = isFuzzy && !hasChild;
+                if (isScaleVisible)
+                    ScaleEditHL.NavigateUrl = string.Format("Fuzzy.aspx?id={0}&isScale=1", critId);
+                return isScaleVisible;
+            }
         }
     }
 }
